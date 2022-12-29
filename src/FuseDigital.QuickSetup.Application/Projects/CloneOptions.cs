1bfd8636bd1435c01ee06b831772fc08812aae6a
Gideon de Swardt <gideon@fusedigital.io>
2022-12-29 10:10:27 +0000

    Update `clone` command help
    Updated the descriptions for the clone verb and options.

--- src/FuseDigital.QuickSetup.Application/Projects/CloneOptions.cs
+++ src/FuseDigital.QuickSetup.Application/Projects/CloneOptions.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Threading.Tasks;
 using CommandLine;
 
 namespace FuseDigital.QuickSetup.Projects;
@@ -10,7 +9,7 @@ public class CloneOptions : IQupCommandOptions
     [Value(0, Required = true, HelpText = "The URL of the repository to clone.")]
     public string Repository { get; set; }
     
-    [Value(1, Required = true, HelpText = "The directory to clone the repository into.")]
+    [Value(1, Required = true, HelpText = "The local path where the repository will be cloned. This can be an absolute or relative path, and the ~ character can be used to represent the user profile folder (which is platform-agnostic).")]
     public string LocalFolder { get; set; }
 
     public Type GetCommandType()
