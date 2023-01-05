f4e69d40f9a21038b4e4a5837f395cbcb6429e03
Gideon de Swardt <gideon@fusedigital.io>
2023-01-05 21:48:39 +0000

    Improve help text
    Improved the help text by adding usage examples for the `install` and
    `update` commands.

--- src/FuseDigital.QuickSetup.Application/PackageManagers/Dto/InstallOptions.cs
+++ src/FuseDigital.QuickSetup.Application/PackageManagers/Dto/InstallOptions.cs
@@ -1,20 +1,37 @@
 using System;
 using System.Collections.Generic;
 using CommandLine;
+using CommandLine.Text;
 
 namespace FuseDigital.QuickSetup.PackageManagers.Dto;
 
-[Verb("install", HelpText = "install stuff")]
+[Verb("install", HelpText = "Install a specific package, packages for the specified package manager, or all package managers if none is specified.")]
 public class InstallOptions : QupCommandOptions
 {
-    [Value(1, MetaName = "package-manager", Required = false, HelpText = "")]
+    [Value(0, MetaName = "package-manager", Required = false, HelpText = "The package manager to install packages for (e.g., pip, npm).")]
     public string PackageManager { get; set; }
 
-    [Value(1, MetaName = "package", Required = false, HelpText = "")]
+    [Value(1, MetaName = "package", Required = false, HelpText = "The package to install and add to the package manager repository.")]
     public IEnumerable<string> Package { get; set; }
 
     public override Type GetCommandType()
     {
         return typeof(InstallCommand);
+    }
+
+    [Usage(ApplicationAlias = ApplicationAlias)]
+    public static IEnumerable<Example> Examples
+    {
+        get
+        {
+            yield return new Example("Install packages for all package managers",
+                new InstallOptions());
+
+            yield return new Example("Install packages for a specific package manager",
+                new InstallOptions {PackageManager = "apt"});
+
+            yield return new Example("Install and track a specific package.",
+                new InstallOptions {PackageManager = "apt", Package = new[] {"curl"}});
+        }
     }
 }
--- src/FuseDigital.QuickSetup.Application/PackageManagers/Dto/UpdateOptions.cs
+++ src/FuseDigital.QuickSetup.Application/PackageManagers/Dto/UpdateOptions.cs
@@ -1,16 +1,31 @@
 using System;
+using System.Collections.Generic;
 using CommandLine;
+using CommandLine.Text;
 
 namespace FuseDigital.QuickSetup.PackageManagers.Dto;
 
-[Verb("update", HelpText = "update stuff")]
+[Verb("update", HelpText = "Updates all packages for the specified package manager, or all package managers if none is specified.")]
 public class UpdateOptions : QupCommandOptions
 {
-    [Value(1, MetaName = "package-manager", Required = false, HelpText = "")]
+    [Value(0, MetaName = "package-manager", Required = false, HelpText = "The package manager to update packages for (e.g., pip, npm).")]
     public string PackageManager { get; set; }
 
     public override Type GetCommandType()
     {
         return typeof(InstallCommand);
+    }
+    
+    [Usage(ApplicationAlias = ApplicationAlias)]
+    public static IEnumerable<Example> Examples
+    {
+        get
+        {
+            yield return new Example("Update packages for all package managers",
+                new UpdateOptions());
+
+            yield return new Example("Update packages for a specific package manager",
+                new UpdateOptions {PackageManager = "apt"});
+        }
     }
 }
